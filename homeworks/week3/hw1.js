9201060f746018fce940c45bcf5a316b10fbeb5a
phwcyn <phwcyn@gmail.com>
2019-05-30 17:25:03 +0800

    hw1.js

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,5 +1,13 @@
 function stars(n) {
-  console.log(n);
+  const result = [];
+  let star = '';
+  for (let i = 0; i < n; i += 1) {
+    star += '*';
+    result.push(star);
+  }
+  return result;
 }
-
+console.log(stars(1));
+console.log(stars(3));
+console.log(stars(6));
 module.exports = stars;
