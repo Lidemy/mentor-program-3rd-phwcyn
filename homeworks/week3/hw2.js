a65e2bf77fbec566a9eca0b35d2e0a5e738d83d4
phwcyn <phwcyn@gmail.com>
2019-05-30 17:33:18 +0800

    hw2.js

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,5 +1,13 @@
 function alphaSwap(str) {
   console.log(str);
+  let Str = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] >= 'a' && str[i] <= 'z') {
+      Str += str[i].toUpperCase();
+    } else {
+      Str += str[i].toLowerCase();
+    }
+  }
+  return Str;
 }
-
 module.exports = alphaSwap;
